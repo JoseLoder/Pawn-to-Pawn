bf0b5c0d894882c0966258c6e81a5e68d072f708
José Miguel Martín Silva <jose.martin@jaraxa.com>
2025-03-07 11:23:18 +0100

    Update client data structure and enhance ClientsTable with action buttons for editing and deleting clients

--- react/src/components/other/ClientsTable.tsx
+++ react/src/components/other/ClientsTable.tsx
@@ -1,11 +1,11 @@
 import { Client } from "../../types/Clients";
 import { Table } from "../semantic/Table";
 
-/* import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { getClientsById, removeClient } from "../../api/clients";
 import { useNavigate } from "react-router";
 import { useContext } from "react";
-import { ProfessionalContext } from "../../contexts/ProfessionalContext"; */
+import { ProfessionalContext } from "../../contexts/ProfessionalContext";
 
 const columns = [
   {
@@ -19,50 +19,62 @@ const columns = [
   {
     path: "phone",
     name: "Phone",
-  }
-]
+  },
+  {
+    path: "actions",
+    name: "Actions",
+  },
+];
 
 export function ClientsTable({ clients }: Readonly<{ clients: Client[] }>) {
-/* 
-  const queryClient = useQueryClient() 
-  const navigate = useNavigate()
+  // Remove client
+  const queryClient = useQueryClient();
 
   const deleteClientMutation = useMutation({
-    mutationKey: ['Delete Client'],
+    mutationKey: ["Delete Client"],
     mutationFn: removeClient,
     onSuccess: () => {
-      alert('Client remove')
-      queryClient.invalidateQueries({ queryKey: ['clients'] }) // Compares the cache and re-triggers the client request
-    }
-  })
+      alert("Client remove");
+      queryClient.invalidateQueries({ queryKey: ["clients"] }); // Compares the cache and re-triggers the client request
+    },
+  });
 
-  const contextProffesionalProvider = useContext(ProfessionalContext)
+  // Get client by id, set in context and navigate to edit client
+  const contextProffesionalProvider = useContext(ProfessionalContext);
   if (!contextProffesionalProvider) {
-    throw new Error("ProfessionalContext must be used within a ProfessionalProvider")
+    throw new Error(
+      "ProfessionalContext must be used within a ProfessionalProvider"
+    );
   }
-  const { handleSetClient } = contextProffesionalProvider
+  const { handleSetClient } = contextProffesionalProvider;
+  const navigate = useNavigate();
 
   const getClientMutation = useMutation({
-    mutationKey: ['Get Client'],
+    mutationKey: ["Get Client"],
     mutationFn: getClientsById,
     onSuccess: (client) => {
-      handleSetClient(client)
-      navigate('/professional/edit-client')
+      handleSetClient(client);
+      navigate("/professional/edit-client");
+    },
+  });
+
+  // Desctructure clients and add actions
+  const clientsWithActions = clients.map((client) => ({
+    ...client,
+    actions: [
+      {
+      name: "Delete",
+      action: () => {
+        deleteClientMutation.mutate(client.id);
+      }
+    },
+    {
+      name: "Edit",
+      action: () => {
+        getClientMutation.mutate(client.id);
+      }
     }
-  })
-
-  const removeRow = (e: React.MouseEvent<HTMLButtonElement>, id: string) => {
-    e.preventDefault()
-    deleteClientMutation.mutate(id)
-  }
-  const getClient = (e: React.MouseEvent<HTMLButtonElement>, id: string) => {
-    e.preventDefault()
-    getClientMutation.mutate(id)
-  } */
-
-  return (
-
-      <Table columns={columns} items={clients} />
-
-  );
+    ],
+  }));
+  return <Table columns={columns} items={clientsWithActions} />;
 }
--- react/src/components/semantic/Table.tsx
+++ react/src/components/semantic/Table.tsx
@@ -1,38 +1,44 @@
 type Columns = {
-    path: string,
-    name: string,
-  }
-  interface IncomingStructureData {
-    columns: Columns[],
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    items: any[];
-  }
-  
+  path: string;
+  name: string;
+};
+interface IncomingStructureData {
+  columns: Columns[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  items: any[];
+}
+
 export function Table({ columns, items }: Readonly<IncomingStructureData>) {
-    return (
-        <table>
-          <thead>
-            <tr>
-              {columns.map((column) => (
-                <th key={column.path + column.name}>{column.name}</th>
-              ))}
-    {/*           <th>Actions</th> */}
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item) => (
-              <tr key={item.id}>
-                {columns.map((column) => (
-                  <td key={column.path + column.name}>{item[column.path]}</td>
-                ))}
-    {/*             <td>
-                  <button onClick={(e: React.MouseEvent<HTMLButtonElement>) => {removeRow(e, item.id)}}>Borrar</button>
-                  <button onClick={(e: React.MouseEvent<HTMLButtonElement>) => {getClient(e, item.id)}}>Edit Client</button>
-                </td> */}
-              </tr>
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column.path + column.name}>{column.name}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            {columns.map((column) => (
+              <td key={column.path}>
+                {column.path === "actions" ? (
+                  <>
+                    {item.actions.map((action: { name: string; action: () => void }) => (
+                      <button key={action.name} onClick={action.action}>
+                        {action.name}
+                      </button>
+                    ))}
+                  </>
+                ) : (
+                  item[column.path]
+                )}
+              </td>
             ))}
-    
-          </tbody>
-        </table>
-      );
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
