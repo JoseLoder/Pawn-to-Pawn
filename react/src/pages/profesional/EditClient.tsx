0cc7cd44ef0eff5ff2b3b83adb24e0ba5424729b
José Miguel Martín Silva <jose.martin@jaraxa.com>
2025-03-10 12:01:59 +0100

    Fix: Unable to edit text fields in EditClient.tsx

--- react/src/pages/profesional/EditClient.tsx
+++ react/src/pages/profesional/EditClient.tsx
@@ -1,74 +1,82 @@
-import { useContext } from "react"
+import { useContext } from "react";
 import { ProfessionalContext } from "../../contexts/ProfessionalContext";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getClients, modifyClient } from "../../api/clients";
 import { Client } from "../../types/Clients";
 
-export function EditClient () {
-    // Get clients
-    const clientListQuery = useQuery({
-        queryKey: ['client'],
-        queryFn: getClients
-    });
+export function EditClient() {
+  // Get clients
+  const clientListQuery = useQuery({
+    queryKey: ["client"],
+    queryFn: getClients,
+  });
 
-    const { data: clients, isLoading } = clientListQuery;
+  const { data: clients, isLoading } = clientListQuery;
 
-    // Modify client
-    const queryClient = useQueryClient();
+  // Modify client
+  const queryClient = useQueryClient();
 
-    const modifyClientMutation = useMutation({
-        mutationKey: ['Modify Client'],
-        mutationFn: modifyClient,
-        onSettled: () => {
-//            clientListQuery.refetch(); Less performance than invalidateQueries
-            queryClient.invalidateQueries({ queryKey: ['client'] });
-        }
-    });
-    const handleModifyClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        const formData = new FormData(e.target as HTMLFormElement);
-        console.log(formData);
-        const client = Object.fromEntries(formData) as Client;
-        console.log(client);
-        modifyClientMutation.mutate(client);
-    }
+  const modifyClientMutation = useMutation({
+    mutationKey: ["Modify Client"],
+    mutationFn: modifyClient,
+    onSettled: () => {
+      //            clientListQuery.refetch(); Less performance than invalidateQueries
+      queryClient.invalidateQueries({ queryKey: ["client"] });
+    },
+  });
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const client = Object.fromEntries(formData) as Client;
+    console.log(client);
+    /*         modifyClientMutation.mutate(client); */
+  };
 
-    // Get client context
-    const professionalClientContext = useContext(ProfessionalContext)
-    if (!professionalClientContext) {
-        throw new Error("ProfessionalContext must be used within a ProfessionalProvider");
-    }
-    const { setClientContext, getClientContext } = professionalClientContext;
+  // Get client context
+  const professionalClientContext = useContext(ProfessionalContext);
+  if (!professionalClientContext) {
+    throw new Error(
+      "ProfessionalContext must be used within a ProfessionalProvider"
+    );
+  }
+  const { setClientContext, getClientContext } = professionalClientContext;
 
-    const clientContext = getClientContext();
+  const clientContext = getClientContext();
 
-    return (
-        isLoading ? (
-            <p>Loading...</p>
-        ) : (
-        <form>
-            <label htmlFor="dni">DNI</label>
-            <select id="dni" value={clientContext?.id ?? ''} onChange={(e) => {
-              const selectedClient = clients.find((client: Client) => client.id === e.target.value);
-              if (selectedClient) {
-                setClientContext(selectedClient);
-              }
-            }}>
-              <option value="" disabled={!clientContext}>Seleccione un cliente</option>
-              {clients.map((client: Client) => (
-                <option key={client.id} value={client.id}>
-                  {client.dni}
-                </option>
-              ))}
-            </select>
-            <label htmlFor="name">Name</label>
-            <input type="text" id='name' value={clientContext?.name} />
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" value={clientContext?.email} />
-            <label htmlFor="phone">Phone</label>
-            <input type="text" id="phone" value={clientContext?.phone} />
-            <button onClick={handleModifyClick}>Modify</button>
-        </form>
-        )
-    )
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="dni">DNI</label>
+      <select
+        id="dni"
+        value={clientContext?.id ?? ""}
+        onChange={(e) => {
+          const selectedClient = clients.find(
+            (client: Client) => client.id === e.target.value
+          );
+          if (selectedClient) {
+            setClientContext(selectedClient);
+          }
+        }}
+      >
+        <option value="" disabled={!clientContext}>
+          Seleccione un cliente
+        </option>
+        {clients.map((client: Client) => (
+          <option key={client.id} value={client.id}>
+            {client.dni}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" value={clientContext?.name} onChange={(e) => {setClientContext({...clientContext, name: e.target.value} as Client)}}/>
+      <label htmlFor="email">Email</label>
+      <input type="text" id="email" value={clientContext?.email} onChange={(e) => {setClientContext({...clientContext, email: e.target.value} as Client)}}/>
+      <label htmlFor="phone">Phone</label>
+      <input type="text" id="phone" value={clientContext?.phone} onChange={(e) => {setClientContext({...clientContext, phone: e.target.value} as Client)}}/>
+      <button>Modify</button>
+    </form>
+  );
 }
