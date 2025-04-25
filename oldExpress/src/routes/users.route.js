9ee71060813d1110411cbdb3c0b29728a40c5075
José Miguel Martín Silva <jose.martin@jaraxa.com>
2025-04-25 13:48:43 +0200

    Delete deprecated oldExpress

--- oldExpress/src/routes/users.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { UserController } from '../controllers/users.controller.js'
-export const usersRouter = Router()
-
-usersRouter.post('/login', UserController.login)
-usersRouter.post('/register', UserController.register)
-usersRouter.post('/logout', UserController.logout)
-usersRouter.delete('/:id', UserController.delete)
--- oldExpress/src/types/users.types.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: string
-  email: string
-  name: string
-  password: string
-}
-export type LogUser = Pick<User, 'email' | 'password'>
-export type RegisterUser = Omit<User, 'id'>
