db26160f7e979fe3a3b2ad64158c2eb618e3c14a
José Miguel Martín Silva <jose.martin@jaraxa.com>
2025-05-05 14:09:53 +0200

    Render only the routes of the logged in user

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,21 +1,58 @@
-import { Nav } from './components/semantic/Nav'
+import { Route, Routes } from 'react-router'
+import { Home } from './pages/Home'
+import { Login } from './pages/home/Login'
+import { Register } from './pages/home/Register'
+import ErrorPage from './pages/ErrorPage'
+import { Client } from './pages/Client'
+import { CreateOrder } from './pages/Client/CreateOrder'
+import { ShowOrders } from './pages/Client/ShowOrders'
+import { Profile } from './pages/Client/Profile'
+import { Professional } from './pages/Professional'
+import { ShowClient } from './pages/profesional/ShowClient'
+import { AddClient } from './pages/profesional/AddClient'
+import { EditClient } from './pages/profesional/EditClient'
+import { Admin } from './pages/Admin'
+import { UserContext } from './contexts/UserContext'
+import { useContext } from 'react'
+
+const RouterMap = {
+  "client":
+    <Route path="client" element={<Client />}>
+      <Route path="create-order" element={<CreateOrder />} />
+      <Route path="show-order" element={<ShowOrders />} />
+      <Route path="me" element={<Profile />} />
+    </Route>
+  ,
+  "operator":
+    <Route path="professional" element={<Professional />}>
+      <Route path="show-client" element={<ShowClient />} />
+      <Route path="add-client" element={<AddClient />} />
+      <Route path="edit-client/" element={<EditClient />} />
+    </Route>,
+  "admin":
+    <Route path="admin" element={<Admin />}>
+    </Route>
+}
+
 function App() {
-  const linksToShow = [
-    { to: "professional", text: "Professional" },
-    { to: "client", text: "Client" },
-    { to: "home", text: "Test New Home" }
-  ]
+  const userContextProvider = useContext(UserContext);
+  if (!userContextProvider) {
+    throw new Error("UserContext must be used within a UserProvider");
+  }
+  const { getUserContext } = userContextProvider;
+
+  const userAlreadyLogged = getUserContext()
+  const role = userAlreadyLogged?.role
 
   return (
-    <>
-      <header className="App-header">
-        <h1>Home</h1>
-        <Nav links={linksToShow} />
-      </header>
-      <main>
-        <h2>Select who you are</h2>
-      </main>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+      {role && RouterMap[role]}
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   )
 }
 
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,21 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
 import styled from "@emotion/styled";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import "./styles.css";
-import { Professional } from "./pages/Professional.tsx";
-import { Home } from "./pages/Home.tsx";
-import { Client } from "./pages/Client.tsx";
-import { Admin } from "./pages/Admin.tsx";
-import { ShowClient } from "./pages/profesional/ShowClient.tsx";
-import { AddClient } from "./pages/profesional/AddClient.tsx";
-import { Login } from "./pages/home/Login.tsx";
-import { Register } from "./pages/home/Register.tsx";
-import { EditClient } from "./pages/profesional/EditClient.tsx";
 import { UserContextProvider } from "./contexts/UserContextProvider.tsx";
-import ErrorPage from "./pages/ErrorPage.tsx";
+
+import App from "./App.tsx";
 
 const StyledRoot = styled.div`
   min-height: 100vh;
@@ -39,24 +30,7 @@ createRoot(document.getElementById("root")!).render(
       <UserContextProvider>
         <BrowserRouter>
           <ReactQueryDevtools />
-          <Routes>
-            <Route path="/" element={<Home />}>
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-            </Route>
-            <Route path="professional" element={<Professional />}>
-              <Route path="show-client" element={<ShowClient />} />
-              <Route path="add-client" element={<AddClient />} />
-              <Route path="edit-client/" element={<EditClient />} />
-            </Route>
-            <Route path="client" element={<Client />}>
-              {/* Routes from client */}
-            </Route>
-            <Route path="admin" element={<Admin />}>
-              {/* Routes from client */}
-            </Route>
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
+          <App />
         </BrowserRouter>
       </UserContextProvider>
     </QueryClientProvider>
