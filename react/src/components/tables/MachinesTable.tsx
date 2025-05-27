6be0aad0e379065df1183556eea2f4d78462f3d3
José Miguel Martín Silva <jose.martin@jaraxa.com>
2025-05-27 23:47:48 +0200

    feat: Created the component to display the machines in a table

---
+++ react/src/components/tables/MachinesTable.tsx
@@ -0,0 +1,28 @@
+import { Table } from "./base/Table";
+
+import { Machine } from "../../types/machines.types";
+
+const columns = [
+  {
+    path: "max_widht",
+    name: "Maximum supported width",
+  },
+  {
+    path: "max_weight",
+    name: "Maximum supported weight",
+  },
+  {
+    path: "max_velocity",
+    name: "Maximum speed achievable",
+  },
+  {
+    path: "price",
+    name: "Price per minute of operation",
+  }
+];
+
+export function MachinesTable({ machines }: Readonly<{ machines: Machine[] }>) {
+  return (
+    <Table columns={columns} items={machines}/>
+  )
+}
